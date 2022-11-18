48ec62dc1108c1397e305ac2b5afdc810eeb93b8
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-11-18 16:52:16 +0530

    oo

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B();
+            b.X();
+        }
+    }
+
+    class A
+    {
+        public void M()
+        {
+            Console.WriteLine("I am M in A");
+        }
+    }
+
+    class B : A
+    {
+        public void M()
+        {
+            Console.WriteLine("I am M in  B");
+        }
+        public void X()
+        {
+            base.M();
+        }
+    }
+}
