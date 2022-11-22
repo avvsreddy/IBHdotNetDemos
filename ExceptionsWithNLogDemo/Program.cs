15026e8cb3a836dd0d1ca91f22def1dee6031394
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-11-22 15:35:59 +0530

    nlog

---
+++ ExceptionsWithNLogDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using NLog;
+
+namespace ExceptionsWithNLogDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // install nuget package: nlog.extenstions.logging
+
+
+            var logger = LogManager.GetCurrentClassLogger();
+            try
+            {
+                int a = 10;
+                int b = 0;
+                var s = a / b;
+                logger.Debug("In main method");
+                Console.WriteLine("done");
+            }
+            catch (Exception ex)
+            {
+                logger.Fatal(ex.Message);
+            }
+
+        }
+    }
+}
