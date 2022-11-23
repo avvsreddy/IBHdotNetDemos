0f1c14a8050dcc4e6a18c25e3a362e8fb05663f6
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-11-23 15:05:54 +0530

    delegates labs

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            while (true)
+            {
+                Console.Write("Enter number: ");
+                string input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                    break;
+                numbers.Add(int.Parse(input));
+            }
+            Console.WriteLine("thanks");
+            Console.WriteLine($"The sum is : {numbers.Sum()}");
+        }
+    }
+}
