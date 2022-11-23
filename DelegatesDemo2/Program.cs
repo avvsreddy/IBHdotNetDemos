f190926e15932c2bd6ff031f46462cf4c2a6957b
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-11-23 12:27:18 +0530

    lambda

--- DelegatesDemo2/Program.cs
+++ DelegatesDemo2/Program.cs
@@ -7,39 +7,87 @@ namespace DelegatesDemo2
         static void Main(string[] args)
         {
             // client 1 - all
-            //ProcessManager.ShowProcessList();
+            //ProcessManager.ShowProcessList(FilterNone);
+
+            // Anonymous Delegates
+            //ProcessManager.ShowProcessList(delegate
+            //    {
+            //        return true;
+            //    });
             // client 2 - by name
             //ProcessManager.ShowProcessList("S");
+            //ProcessManager.ShowProcessList(FilterByName);
+            ProcessManager.ShowProcessList(delegate (Process p)
+            {
+                return p.ProcessName.StartsWith("S");
+            });
             // client 3 - by memory
-            ProcessManager.ShowProcessList(100 * 1024 * 1024);
+            //FilterDelegate filter = new FilterDelegate(FilterByMem);
+            //ProcessManager.ShowProcessList(filter);
+
+            // Anonymous Delegates
+
+            ProcessManager.ShowProcessList(delegate (Process p)
+            {
+                return (p.WorkingSet64 >= 100 * 1024 * 1024);
+            });
+
+            // Lambda - Statement
+
+            ProcessManager.ShowProcessList((Process p) =>
+                {
+                    return (p.WorkingSet64 >= 100 * 1024 * 1024);
+                });
+
+            // Lambda - Expression - Light Weight syntax for anonymous delegates
+
+            ProcessManager.ShowProcessList(p => p.WorkingSet64 >= 100 * 1024 * 1024);
+
+
+            // client 4
+            //ProcessManager.ShowProcessList(FilterByThreads);
 
         }
+
+        // client 1
+        //public static bool FilterNone(Process p)
+        //{
+        //    return true;
+        //}
+
+
+        // client 2
+        //public static bool FilterByName(Process p)
+        //{
+        //    return p.ProcessName.StartsWith("S");
+        //}
+
+        // client 3
+        public static bool FilterByMem(Process p)
+        {
+            return (p.WorkingSet64 >= 100 * 1024 * 1024);
+        }
+
+        // client 4
+        public static bool FilterByThreads(Process p)
+        {
+            return p.Threads.Count >= 50;
+        }
+
     }
 
+
+    public delegate bool FilterDelegate(Process p);
     public class ProcessManager
     {
-        public static void ShowProcessList()
+        public static void ShowProcessList(FilterDelegate filter)
         {
             foreach (Process p in Process.GetProcesses())
             {
-                Console.WriteLine(p.ProcessName);
-            }
-        }
-        public static void ShowProcessList(string sw)
-        {
-            foreach (Process p in Process.GetProcesses())
-            {
-                if (p.ProcessName.StartsWith(sw))
+                if (filter.Invoke(p))
                     Console.WriteLine(p.ProcessName);
             }
         }
-        public static void ShowProcessList(long memSize)
-        {
-            foreach (Process p in Process.GetProcesses())
-            {
-                if (p.WorkingSet64 >= memSize)
-                    Console.WriteLine(p.ProcessName);
-            }
-        }
+
     }
 }
