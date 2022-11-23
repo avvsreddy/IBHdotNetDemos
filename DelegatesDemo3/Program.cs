8e75635db94e358d9edf253fb859fa044366a3dc
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-11-23 12:57:42 +0530

    delegates

---
+++ DelegatesDemo3/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace DelegatesDemo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account();
+            acc1.Deposit(5000);
+            Console.WriteLine(acc1.Balance);
+            acc1.Withdraw(1000);
+            Console.WriteLine(acc1.Balance);
+        }
+    }
+
+    class Account
+    {
+        public int Balance { get; private set; }
+
+        public void Deposit(int amount)
+        {
+            Balance += amount;
+            Notification.SendMail($"Account has been increased by {amount}");
+
+        }
+
+        public void Withdraw(int amount)
+        {
+            Balance -= amount;
+            // write a code send email
+            Notification.SendMail($"Account has been decreased by {amount}");
+        }
+    } // OCP
+
+
+    class Notification
+    {
+        public static void SendMail(string msg)
+        {
+            Console.WriteLine($"Mail: {msg}");
+        }
+    }
+}
---
+++ LambdaDemo1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace LambdaDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Func<int, bool> filter = new Func<int, bool>(IsEven);
+            int sum = numbers.Where(filter).Sum();
+
+            sum = numbers.Where(IsEven).Sum();
+
+            sum = numbers.Where(x => x % 2 == 0).Sum();
+            sum = (from n in numbers
+                   where n % 2 == 0
+                   select n).Sum();
+
+            int evenSum = 0;
+
+            foreach (var item in numbers)
+            {
+                if (item % 2 == 0)
+                    evenSum += item;
+            }
+
+            Console.WriteLine(evenSum);
+
+        }
+
+        static bool IsEven(int n)
+        {
+            return n % 2 == 0;
+        }
+    }
+}
