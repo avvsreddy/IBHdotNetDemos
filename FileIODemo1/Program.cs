c6a8b87a7765e80c49dd5a5b28a4859c0b380c02
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-11-24 11:10:37 +0530

    file io

---
+++ FileIODemo1/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace FileIODemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //WriteFile();
+
+            //ReadAll();
+
+            // read line by line
+            // read data from file
+            StreamReader sr = new StreamReader("e://sample//test.txt");
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            sr.Close();
+
+
+
+        }
+
+        private static void ReadAll()
+        {
+            // read data from file
+            StreamReader sr = new StreamReader("e://sample//test.txt");
+            string allLines = sr.ReadToEnd();
+            sr.Close();
+            Console.WriteLine(allLines);
+        }
+
+        private static void WriteFile()
+        {
+            StreamWriter sw = null;
+            try
+            {
+                Console.Write("Enter your name: ");
+                string name = Console.ReadLine();
+                // store the name into file
+                sw = new StreamWriter("e://sample//test.txt", true);
+                sw.WriteLine(name);
+                // sdfsdfd
+                //sdfsdfsdf
+                //sdfsdfsdf
+            }
+            finally
+            {
+                sw.Close();
+            }
+        }
+    }
+}
