efdd3df19add8aebcdb64e9ab51a55eff6a9d429
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-11-29 10:56:57 +0530

    mt

--- MTDemo2/Program.cs
+++ MTDemo2/Program.cs
@@ -8,13 +8,22 @@
             Task tt1 = new Task(M1);
             Parallel.Invoke(M1);
 
-            Thread t2 = new Thread(M2);//error
+            //Thread t2 = new Thread(M2);//error
+            Thread t2 = new Thread(() => { M2(100); });
+            Task tt2 = new Task(() => { M2(1); });
+
+            //Thread t3 = new Thread(M3);
+            Task<int> tt3 = new Task<int>(M3);
+            tt3.Start();
+            int result = tt3.Result;
+
+            Task<int> tt4 = new Task<int>(() => { return M4(100); });
         }
 
 
         static void M1() { }
         static void M2(int a) { }
-        static int M3() { retrun 1; }
+        static int M3() { return 1; }
         static int M4(int a) { return a; }
     }
 }
---
+++ MTDemo4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+
+namespace MTDemo4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Image Flipping App");
+            Console.WriteLine("Rotating Images seq...");
+            Stopwatch sw = Stopwatch.StartNew();
+            RotateImage1();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+            Console.WriteLine("Rotating Images Parallel....");
+            sw.Restart();
+            Parallel.Invoke(RotateImage2);
+            Console.WriteLine(sw.ElapsedMilliseconds);
+
+        }
+
+        static void RotateImage1()
+        {
+            var images = Directory.GetFiles("E:\\pictures");
+            foreach (var img in images) // Seq
+            {
+                Bitmap bmp = new Bitmap(img);
+                bmp.RotateFlip(RotateFlipType.Rotate180FlipX);
+                FileInfo finfo = new FileInfo(img);
+                bmp.Save($"E:\\rotatedpictures\\{finfo.Name}");
+            }
+        }
+
+        static void RotateImage2()
+        {
+            var images = Directory.GetFiles("E:\\pictures");
+            //foreach (var img in images) // Seq
+            Parallel.ForEach(images, img =>
+            {
+                Bitmap bmp = new Bitmap(img);
+                bmp.RotateFlip(RotateFlipType.Rotate180FlipX);
+                FileInfo finfo = new FileInfo(img);
+                bmp.Save($"E:\\rotatedpictures\\{finfo.Name}");
+            });
+        }
+    }
+}
