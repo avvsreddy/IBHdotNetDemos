ef9bca76f385179985c6f4c47d7d3588b7523b47
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2022-12-14 12:54:50 +0530

    mvc

---
+++ KnowledgeHubPortalWebApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using KnowledgeHubPortalWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace KnowledgeHubPortalWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        // http://diasdfsdfsdf.com/home/index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+        public IActionResult abcd()
+        {
+            ViewBag.Message = "I am abcd";
+            return View();
+        }
+    }
+}
---
+++ KnowledgeHubPortalWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KnowledgeHubPortalWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
